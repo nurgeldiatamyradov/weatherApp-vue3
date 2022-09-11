296505958d4f3ed12eefa388f6092705815e7127
nurgeldi atamyradov <67277491+nurgeldiatamyradov@users.noreply.github.com>
2022-09-11 17:37:32 +0200

    waiting openweather API activation

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,11 +1,12 @@
 <template>
-  
+  <div>
+    <AsyncCityView />
+  </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import AsyncCityView from '../components/AsyncCityView.vue';
 
-}
 </script>
 
 <style>
